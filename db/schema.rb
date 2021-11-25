cce97d0141b7bd934d75ae588fb5b88f8bec43e2
Aman Laxane <laxaneaman@gmail.com>
2021-11-25 16:46:59 +0530

    added one to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_095835) do
+ActiveRecord::Schema.define(version: 2021_11_25_110935) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_095835) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "Author"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
