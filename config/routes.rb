5d69fe776b298ec036461b0f86a803f11e28ecee
Aman Laxane <laxaneaman@gmail.com>
2021-11-26 17:55:07 +0530

    added signup pags

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root'articles#home'
 resources :articles, only:[:show,:index, :new, :create, :edit, :update, :destroy]
-
+get 'signup' , to: 'users#new'
+post 'users', to: 'users#create'
 end
